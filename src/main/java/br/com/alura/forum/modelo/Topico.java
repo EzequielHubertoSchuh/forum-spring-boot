6ae6242c5fd64f0d6023f8ff763866bf162e87ad
EzequielHubertoSchuh <ezequiel_utf@yahoo.com.br>
2022-02-09 20:36:41 -0300

    second commit

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -14,7 +14,7 @@ public class Topico {
 	private Usuario autor;
 	private Curso curso;
 	private List<Resposta> respostas = new ArrayList<>();
-
+	
 	public Topico(String titulo, String mensagem, Curso curso) {
 		this.titulo = titulo;
 		this.mensagem = mensagem;
