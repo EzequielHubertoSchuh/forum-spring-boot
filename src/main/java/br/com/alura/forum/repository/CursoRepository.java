3f067580378c1d9ffbdcbd17fd2a5e775e9d7923
EzequielHubertoSchuh <ezequiel_utf@yahoo.com.br>
2022-02-09 23:56:59 -0300

    commit

--- src/main/java/br/com/alura/forum/repository/CursoRepository.java
+++ src/main/java/br/com/alura/forum/repository/CursoRepository.java
@@ -1,7 +1,8 @@
 package br.com.alura.forum.repository;
 
-import br.com.alura.forum.modelo.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Curso;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 
